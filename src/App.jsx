9db31c164627b39846c0648d248e1d9a223b138e
Your Name <you@example.com>
2024-06-29 02:02:53 +0500

    add

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+"use client";
+
+import StartGame from "./components/StartGame";
+
+const App = () => {
+  return (
+    <div className="w-[100vw] h-[100vh] " >
+        <StartGame/>
+    </div>
+  );
+};
+
+export default App;
